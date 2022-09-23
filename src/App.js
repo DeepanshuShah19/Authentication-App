c6a4f22ab0c1ab335b769df46d1c37629275a450
jaineel dharia <jaineeldharia123@gmail.com>
2022-09-23 11:49:51 -0400

    Frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomePage from "./components/HomePage/HomePage"
+import LoginPage from "./components/LoginPage/LoginPage"
+import RegistrationPage from './components/RegistrationPage/RegistrationPage';
+
+function App() {
+  return (
+    <div className="App">
+        <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
