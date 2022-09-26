7870a8f11ba0783a034f4f333308f9df4f1a162a
Deepanshu Shah <deepanshushah@Deepanshus-MacBook-Air-2.local>
2022-09-26 15:50:25 -0400

    backend code for user profile

--- src/App.js
+++ src/App.js
@@ -1,86 +1,15 @@
 import './App.css';
 import { useState } from 'react';
 import HomePage from "./components/HomePage/HomePage"
-import LoginPage from "./components/LoginPage/LoginPage"
-import RegistrationPage from './components/RegistrationPage/RegistrationPage';
-import EditableUserProfile from './components/EditableUserProfile';
-import UserProfile from './components/UserProfile';
-
-const animals = [
-  "Aardvark",
-  "Albatross",
-  "Alpaca",
-  "Alligator",
-  "Anchovie",
-  "Angelfish",
-  "Ant",
-  "Antelope",
-  "Armadillo",
-  "Aurochs",
-  "Axolotl"
-]
-
-function randomColor() {
-  return "#" + Math.floor(Math.random()*16777215).toString(16);
-}
-
-function randomName() {
-  
-  return "Anonymous " + animals[Math.floor(Math.random() * animals.length)]
-}
-
+import LoginRegestration from './components/Loginregestration';
+import userProfile from './components/UserProfile'
 function App() {
-  const now = new Date(Date.now());
-    const defaultBirthday = new Date(now.getTime() + 86400000);
-
-    const [editMode, setEditMode] = useState(false);
-
-    const [name, setName] = useState(randomName());
-    const [month, setMonth] = useState(defaultBirthday.getMonth());
-    const [day, setDay] = useState(defaultBirthday.getDate());
-    const [color, setColor] = useState(randomColor());
-
-    const stored = {name, month, day, color};
-    const isBirthdayToday = now.getMonth() === month && now.getDate() === day;
-
-    function handleEditComplete(result) {
-        console.log("handleEditComplete", result);
-        if (result != null) {
-            setName(result.name);
-            setMonth(result.month);
-            setDay(result.day);
-            setColor(result.color);
-        }        
-        setEditMode(false);
-    }
   return (
     <div className="container">
-      <LoginPage />
-            {/* <div className="App">                 
-                {
-                    editMode
-                        ? <>
-                            <h1>My Profile</h1>
-                            <EditableUserProfile
-                                    stored={stored}
-                                    editCompleteCallback={handleEditComplete}                            
-                            />
-                        </>
-                        : <>
-                            {
-                                isBirthdayToday
-                                    ? <div className="birthday">Happy Birthday!</div>
-                                    : <h1>My Profile</h1>
-                            }
-                            <UserProfile
-                                    stored={stored}
-                                    startEditCallback={() => setEditMode(true)}
-                            />
-                        </>
-                }            
-                
-            </div> */}
-        </div>
+      {/* <LoginRegestration /> */}
+      <HomePage/>
+      {/* <userProfile/> */}
+    </div>
         
   );
 }
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,16 @@
 import React from "react"
 import "./HomePage.css"
 
-const HomePage = () => {
+const HomePage = () => {    
     return (
         <div className = "HomePage">
-            <h1>Hello Homepage</h1>
+            <h1>Profile</h1>
+            <input type="text" name="Name" placeholder="Enter your Name"></input>
+            <input type="text" name="Bio"  placeholder="Enter your Information" ></input>
+            <input type="text" name="Phone Number"  placeholder="Enter your Phone Number" ></input>
+            <input type="text" name="Email ID" placeholder="Email ID"></input>
+            <input type="Password" name="Password" placeholder="Password"></input>
+            <div className="button">Edit</div>
             <div className="button">Logout</div>
         </div>
     )
